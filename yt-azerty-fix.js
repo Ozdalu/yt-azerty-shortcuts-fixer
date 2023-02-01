2f109d19a9652a962da728f645246603efdfc54b
Ormazd <pbisset@protonmail.com>
2023-02-01 11:38:01 +0100

    Code cleanup + Bug fix

--- yt-azerty-fix.js
+++ yt-azerty-fix.js
@@ -1,46 +1,45 @@
+const keyRemapping = new Map()
+keyRemapping.set(59, 190)
+keyRemapping.set(169, 189)
+keyRemapping.set(90, 87)
+keyRemapping.set(81, 65)
+keyRemapping.set(164, 221)
+keyRemapping.set(0, 219)
+keyRemapping.set(160, 219)
+
 function keyUpping(original, faked) {
     document.addEventListener('keyup', (ev) => {
-        if (ev.keyCode == original) {
-            document.dispatchEvent(new KeyboardEvent("keyup", {
-                'shiftKey': false,
-                'keyCode': faked
-            }))
-        }
+        // Avoids recursion
+        if (ev.keyCode != original) return
+
+        document.dispatchEvent(new KeyboardEvent("keyup", {
+            'shiftKey': false,
+            'keyCode': faked
+        }))
     })
-    return 1
+    return true
 }
 
 document.addEventListener('keydown', (e) => {
     e.preventDefault
     const el = document.activeElement.id
-    if (el != "search" && el != "contenteditable-root") {
-        e = e || window.event;
-        switch (e.keyCode) {
-            case 59:
-            case 169:
-                // Next Frame (while paused) + Increase playback rate 59
-                // Rotate through font sizes (decreasing) 169
-                var kcode = e.keyCode == 59 ? 190 : 173
-                document.dispatchEvent(new KeyboardEvent("keydown", {
-                    'shiftKey': e.shiftKey,
-                    'keyCode': kcode
-                }))
-                break
-            case 90:
-            case 81:
-            case 164:
-            case 160:
-                // Pan up 90 / left 81
-                // Zoom in 164 / out 160
-                var kcode = e.keyCode == 90 ? 87 : e.keyCode == 81 ? 65 : e.keyCode == 164 ? 221 : 219
-                do {
-                    document.dispatchEvent(new KeyboardEvent("keydown", {
-                        'shiftKey': false,
-                        'keyCode': kcode
-                    }))
-                }
-                while (!keyUpping(e.keyCode, kcode))
-                break
-        }
+    if (el == "search" || el == "contenteditable-root") return
+    
+    // Stops if not a key that has to be fixed
+    e = e || window.event;
+    if (!keyRemapping.has(e.keyCode)) return
+
+    // Setting up Shift key value
+    let shiftKey = false
+    if (e.keyCode == 59 || e.keyCode == 169) {
+        shiftKey = e.shiftKey;
     }
+
+    do {
+        document.dispatchEvent(new KeyboardEvent("keydown", {
+            'shiftKey': shiftKey,
+            'keyCode': keyRemapping.get(e.keyCode)
+        }))
+    }
+    while (!keyUpping(e.keyCode, keyRemapping.get(e.keyCode)))
 })
